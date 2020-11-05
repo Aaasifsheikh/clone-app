f74906c7a297b214466242983d626a96a5216acc
Aasif Sheikh <Aasifsheikh820@gmail.com>
2020-11-05 08:06:50 +0530

    Updates

---
+++ precache-manifest.7f58d436b43541325c1e71c3e7cbef14.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654da1dbc5c6c9305b7162f8d1980ed3",
+    "url": "/clone-app/index.html"
+  },
+  {
+    "revision": "76daf2a33ba5ea35f0e3",
+    "url": "/clone-app/static/css/main.6707fe41.chunk.css"
+  },
+  {
+    "revision": "0561252b61dd9f602864",
+    "url": "/clone-app/static/js/2.e801d7b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clone-app/static/js/2.e801d7b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76daf2a33ba5ea35f0e3",
+    "url": "/clone-app/static/js/main.08b8fdd5.chunk.js"
+  },
+  {
+    "revision": "21dcfae6ca363d1ec634",
+    "url": "/clone-app/static/js/runtime-main.52d9e50c.js"
+  },
+  {
+    "revision": "980db521bd947311fefbe34196415e79",
+    "url": "/clone-app/static/media/Book Image 2020-10-05 at 11.28.14 AM.980db521.jpeg"
+  },
+  {
+    "revision": "d6e1dbd57689e213a78ff05572aac00f",
+    "url": "/clone-app/static/media/Laptop Image 2020-10-05 at 3.06.03 PM.d6e1dbd5.jpeg"
+  },
+  {
+    "revision": "503c967b12473952adf8e8fc4b675011",
+    "url": "/clone-app/static/media/Mobile Image 2020-10-05 at 3.03.19 PM.503c967b.jpeg"
+  },
+  {
+    "revision": "2c7c1b6970ec3a8ec0662794d4f18086",
+    "url": "/clone-app/static/media/Tv Image 2020-10-05 at 3.08.12 PM.2c7c1b69.jpeg"
+  },
+  {
+    "revision": "446e936afb0fd987672fcc825c6b97dd",
+    "url": "/clone-app/static/media/VibImage 2020-10-05 at 3.15.48 PM.446e936a.jpeg"
+  },
+  {
+    "revision": "7e99f26a6c8e53f889c99a2ac03c4352",
+    "url": "/clone-app/static/media/WhatsApp Image 2020-10-05 at 10.07.50 AM.7e99f26a.jpeg"
+  },
+  {
+    "revision": "bf65cfa794d6ec4a98a98149c564da80",
+    "url": "/clone-app/static/media/Wm Image 2020-10-05 at 3.12.21 PM.bf65cfa7.jpeg"
+  },
+  {
+    "revision": "784842620668cb82ec2a7ab1a0a40003",
+    "url": "/clone-app/static/media/amaazon-ad.78484262.jpeg"
+  },
+  {
+    "revision": "7451f65b8ffd3f20732b3ac43e21942a",
+    "url": "/clone-app/static/media/amazon_PNG11.7451f65b.png"
+  },
+  {
+    "revision": "ef81cd43c0b24dd1759c8abd848a4c05",
+    "url": "/clone-app/static/media/amazon_PNG24.ef81cd43.png"
+  }
+]);
